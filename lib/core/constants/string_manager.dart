01dddcd3264f8876890deb790fbb0502756f98c5
Ahmed <benzemaa46@gmail.com>
2026-02-19 21:24:44 +0200

    feat(auth): add login string constants and logo asset

--- lib/core/constants/string_manager.dart
+++ lib/core/constants/string_manager.dart
@@ -1,3 +1,13 @@
 class StringManager {
-  static const String appName = 'E-Commerce';
+  static const String appName = 'E-Market';
+  static const String subTitleLoginPage = 'Sign in to continue shopping';
+  static const String email = 'Email';
+  static const String emailHint = 'Enter your email';
+  static const String password = 'Password';
+  static const String passwordHint = 'Enter your password';
+  static const String rememberMe = 'Remember me';
+  static const String login = 'Login';
+  static const String forgotPassword = 'Forgot password?';
+  static const String signUp = 'Sign up';
+  static const String dontHaveAccount = 'Don\'t have an account?';
 }
--- lib/feature/auth/login/presentation/login_screen.dart
+++ lib/feature/auth/login/presentation/login_screen.dart
@@ -5,6 +5,9 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Column(children: [
+          
+        ],
+      ));
   }
 }
