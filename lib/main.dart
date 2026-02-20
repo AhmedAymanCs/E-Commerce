5b2a24247b111ac6b1ba636b5312613104367f60
Ahmed <benzemaa46@gmail.com>
2026-02-20 16:15:54 +0200

    setup service locator for secure storage

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupDioLocator();
+  setupSeucreStorageLocator();
   setupAuthRepositoryLocator();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
