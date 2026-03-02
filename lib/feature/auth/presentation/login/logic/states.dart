69d44a47172da7bef97f3d45280f0fa20d1cd4fc
Ahmed <benzemaa46@gmail.com>
2026-03-02 06:00:26 +0200

    delete old comments in login states file

--- lib/feature/auth/presentation/login/logic/states.dart
+++ lib/feature/auth/presentation/login/logic/states.dart
@@ -1,27 +1,6 @@
 import 'package:e_commerce/core/models/user_model.dart';
 import 'package:equatable/equatable.dart';
 
-// abstract class LoginStates {}
-
-// class LoginInitialState extends LoginStates {}
-
-// class LoginLoadingState extends LoginStates {}
-
-// class LoginSuccessState extends LoginStates {
-//   final UserModel userModel;
-//   LoginSuccessState(this.userModel);
-// }
-
-// class LoginErrorState extends LoginStates {
-//   final String errorMessage;
-
-//   LoginErrorState(this.errorMessage);
-// }
-
-// class ChangePasswordVisibleState extends LoginStates {}
-
-// class ChangeRememberMeState extends LoginStates {}
-
 abstract class FormStatus extends Equatable {
   const FormStatus();
 }
